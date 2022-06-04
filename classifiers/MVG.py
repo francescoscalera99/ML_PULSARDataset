5c3f7f7b438defe34d810a3c62efc136a09f4e09
Riccardo Sepe <seric814@gmail.com>
2022-06-04 19:57:14 +0200

    Minor changes

--- classifiers/MVG.py
+++ classifiers/MVG.py
@@ -7,11 +7,14 @@ from utils.matrix_utils import covariance_matrix_mean, vrow, vcol
 
 class MVG(ClassifierClass):
     class Model(ClassifierClass.Model):
-        def __init__(self, mu0, c0, mu1, c1=None, variant='full-cov'):
+        def __init__(self, mu0: np.ndarray, c0: np.ndarray, mu1: np.ndarray, c1: np.ndarray = None, variant='full-cov'):
             self.mu0 = mu0
             self.mu1 = mu1
             if variant == 'tied':
                 self.c = c0
+            elif variant == 'diag':
+                self.c0 = c0 * np.eye(c0.shape[0])
+                self.c1 = c1 * np.eye(c1.shape[0])
             else:
                 self.c0 = c0
                 self.c1 = c1
@@ -26,17 +29,18 @@ class MVG(ClassifierClass):
         c1, mu1 = covariance_matrix_mean(self.training_data[:, self.training_labels == 1])
 
         if self.variant == 'tied':
-            c = tied_covariance_matrix(self.training_data, self.training_labels)
+            c = within_class_variability(self.training_data, self.training_labels)
             self._model = MVG.Model(mu0, c, mu1, variant=self.variant)
-
-        elif self.variant == 'diag':
-            pass
         else:
             self._model = MVG.Model(mu0, c0, mu1, c1, variant=self.variant)
 
     def classify(self, testing_data: np.ndarray, priors: np.ndarray) -> np.ndarray:
-        class0_conditional_probability = self.logpdf_GAU_ND(testing_data, self._model.mu0, self._model.c0)
-        class1_conditional_probability = self.logpdf_GAU_ND(testing_data, self._model.mu1, self._model.c1)
+        if self.variant == 'tied':
+            class0_conditional_probability = self.logpdf_GAU_ND(testing_data, self._model.mu0, self._model.c)
+            class1_conditional_probability = self.logpdf_GAU_ND(testing_data, self._model.mu1, self._model.c)
+        else:
+            class0_conditional_probability = self.logpdf_GAU_ND(testing_data, self._model.mu0, self._model.c0)
+            class1_conditional_probability = self.logpdf_GAU_ND(testing_data, self._model.mu1, self._model.c1)
         logScores = np.vstack((class0_conditional_probability.ravel(), class1_conditional_probability.ravel()))
         logJoint = logScores + vcol(np.log(priors))
         logMarginal = vrow(scipy.special.logsumexp(logJoint, axis=0))
@@ -60,15 +64,37 @@ class MVG(ClassifierClass):
 def tied_covariance_matrix(D, L):
     D0 = D[:, L == 0]
     D1 = D[:, L == 1]
-    D2 = D[:, L == 2]
 
     C0 = covariance_matrix_mean(D0)[0]
     C1 = covariance_matrix_mean(D1)[0]
-    C2 = covariance_matrix_mean(D2)[0]
 
-    SW = D0.shape[1] * C0 + D1.shape[1] * C1 + D2.shape[1] * C2
+    SW = D0.shape[1] * C0 + D1.shape[1] * C1
     return SW / D.shape[1]
 
+def within_class_variability(dataset, labels) -> np.ndarray:
+    """
+    Evaluates the within class covariance given the labeled dataset
+
+    :param dataset: the dataset (unlabeled)
+    :param labels: the labels of :param dataset
+    :return: the within class covariance matrix
+    """
+    num_classes = len(np.unique(labels))
+    n_features, n_samples = dataset.shape
+    sw = np.zeros((n_features, n_features))
+
+    for i in range(num_classes):
+        class_data = dataset[:, labels == i]
+        c_mean = class_mean(dataset, labels, i)
+
+        class_data = class_data - vcol(c_mean)
+        sw = sw + class_data @ class_data.T
+
+    return sw / n_samples
+
+def class_mean(dataset, labels, c):
+    data = dataset[:, labels == c]
+    return np.mean(data, axis=1)
 
 def main():
     pass
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from utils.matrix_utils import vrow, vcol
 from utils.utils import load_dataset, \
                   z_normalization, \
                   gaussianize, \
@@ -26,11 +27,32 @@ def main():
     # create_heatmap(gaussianized_dtr[:, ltr == 1], cmap="Blues", title="True class")
     # create_heatmap(gaussianized_dtr[:, ltr == 0], cmap="Greens", title="False class")
 
-    mvg = MVG(gaussianized_dtr, ltr)
+    mvg = MVG(gaussianized_dtr, ltr, variant='tied')
     mvg.train_model()
-    predictions = mvg.classify(dte, np.array([0.5, 0.5]))
+    predictions = mvg.classify(dte, np.array([0.9, 0.1]))
     acc, err = compute_accuracy(predictions, lte)
     print(round(err*100, 2), '%')
+    cm = build_confusion_matrix(vcol(lte), vcol(predictions))
+    print(cm)
+
+
+def build_confusion_matrix(testing_labels: np.ndarray, predicted_labels: np.ndarray) -> np.ndarray:
+    if testing_labels.size != predicted_labels.size:
+        raise RuntimeError("Testing labels array and predicted labels array should have the same size.\n"
+                           f"Got instead 'len(testing_labels)'={len(testing_labels)},"
+                           f" 'len(predicted_labels)'={len(predicted_labels)}")
+
+    num_classes = np.unique(testing_labels).size
+    num_samples = testing_labels.size
+    predicted_labels = list(predicted_labels)
+    testing_labels = list(testing_labels)
+
+    cf = np.zeros((num_classes, num_classes), dtype=int)
+
+    for i in range(num_samples):
+        cf[predicted_labels[i], testing_labels[i]] += 1
+
+    return cf
 
 
 if __name__ == '__main__':
