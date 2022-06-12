f635e23c6ccf1d43acb185f92c6b6e0c47201799
Riccardo Sepe <seric814@gmail.com>
2022-06-12 20:26:03 +0200

    Implemented parameter tuning by francesco.scalera

--- main.py
+++ main.py
@@ -87,6 +87,68 @@ def LR_simulations(training_data, training_labels):
     print(table)
 
 
+def LR_simulations(training_data, training_labels):
+    # datas = [training_data, z_normalized_training_data, z_gauss_training_data]
+    # data_types = ['raw', 'z-normalized', 'z-normalized + gaussianized']
+    # ds = list(range(3))
+    # m = [None, 7, 5, 4]
+    m = [None, 7, 5, 4]
+    pis = [0.1, 0.5, 0.9]
+    pis_T = [0.5, 0.1, 0.9]
+    pis = [0.5, 0.1, 0.9]
+    lbd = 1e-3
+
+    hyperparameters = itertools.product(m, pis, pis_T)
+
+    table = PrettyTable()
+    table.field_names = ['Hyperparameters', 'min DCF']
+
+    for m, pi, pi_T in hyperparameters:
+        if m is not None:
+            training_data = PCA(training_data, m)
+
+        llrs, labels = k_fold(training_data, training_labels, LR, 5, seed=0, lbd=lbd, pi_T=pi_T)
+
+        min_dcf = compute_min_DCF(np.array(llrs), labels, pi, 1, 1)
+        table.add_row([f"PCA m={m}, data: gaussianized, π_tilde={pi}, π_T={pi_T}  λ={lbd}", round(min_dcf, 3)])
+
+    print(table)
+
+
+def tuning_parameters_PolySVM(training_data, training_labels):
+    titles_Kfold = ['Gaussianized feature (5-fold, no PCA)', 'Guassianized feature (5-fold, PCA = 7)', 'Gaussianized feature (5-fold, PCA = 5)']
+
+    datasets = []
+
+    training_dataPCA7 = PCA(training_data, 7)
+    training_dataPCA5 = PCA(training_data, 5)
+    datasets.append(training_data, training_dataPCA7, training_dataPCA5)
+
+    C_values = np.logspace(-3, 3, 20)
+    K_values = [0.0, 1.0, 10.0]
+    c_values = [0, 1, 10, 15]
+
+    hyperparameters = itertools.product(C_values, c_values, K_values)
+    j = 0
+    for dataset in datasets:
+        i = 0
+        plt.figure()
+        plt.rcParams['text.usetex'] = True
+        for c, K in hyperparameters:
+            DCFs = []
+            for C in C_values:
+                llrs = k_fold(dataset, training_labels, SVM, 5, seed=0, k=K, c=C, kernel_params=(2, c), kernel_type='poly')
+                min_dcf = compute_min_DCF(llrs, training_labels, 0.5, 1, 1)
+                print("min_DCF for C = ", C, "with c = ", c, "and K =", K)
+                DCFs.append(min_dcf)
+            # f"prior:0.5, c:{c}, K:{K}"
+            plt.plot(C_values, DCFs, color=np.random.rand(3,), label=r"$\pi_{}T=0.5$, c="+str(c)+r", K="+str(K))
+        plt.title(titles_Kfold[j])
+        j += 1
+        plt.legend()
+        plt.xscale('log')
+        plt.show()
+
 def main():
     (training_data, training_labels), _ = load_dataset()
     # find_optLambda(training_data, training_labels)
@@ -103,18 +165,18 @@ def main():
     # plt.show()
     #
     # MVG_simulations(training_data, training_labels)
-    (dtr, ltr), (dte, lte) = splitData_SingleFold(training_data, training_labels, seed=0)
-
-    dtr = gaussianize(dtr, dtr)
-    dte = gaussianize(dtr, dte)
-
-    classifier = SVM(dtr, ltr, k=1, c=5e-5, kernel_params=(2, 15), kernel_type='poly')
-    classifier.train_model()
-    classifier.classify(dte, None)
-    llrs = classifier.get_llrs()
-    min_dcf = compute_min_DCF(np.array(llrs), lte, 0.5, 1, 1)
-    print(min_dcf)
-
+    # (dtr, ltr), (dte, lte) = splitData_SingleFold(training_data, training_labels, seed=0)
+    #
+    # dtr = gaussianize(dtr, dtr)
+    # dte = gaussianize(dtr, dte)
+    #
+    # classifier = SVM(dtr, ltr, k=1, c=5e-5, kernel_params=(2, 15), kernel_type='poly')
+    # classifier.train_model()
+    # classifier.classify(dte, None)
+    # llrs = classifier.get_llrs()
+    # min_dcf = compute_min_DCF(np.array(llrs), lte, 0.5, 1, 1)
+    # print(min_dcf)
+    tuning_parameters_PolySVM(training_data, training_labels)
 
 
 if __name__ == '__main__':
